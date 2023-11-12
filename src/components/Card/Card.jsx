8fdda8f5e1e811debd0311563299cb8b9428b7e8
Saurabh Kumar <saukumar95@gmail.com>
2023-11-12 17:50:29 +0530

    Added login with google and fomatter config

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,15 @@
-import "./Card.css"
+import './Card.css'
 const Card = ({ title, subtitle, children, footer }) => {
     return (
         <div className="card">
             <div>
-                <div className='title'>{title}</div>
+                <div className="title">{title}</div>
                 {subtitle && <span className="subtitle">{subtitle}</span>}
             </div>
-            <form className='body'>
-                {children}
-            </form>
-            <div className="footer">
-                {footer}
-            </div>
+            <form className="body">{children}</form>
+            <div className="footer">{footer}</div>
         </div>
     )
 }
 
-export default Card;
+export default Card
